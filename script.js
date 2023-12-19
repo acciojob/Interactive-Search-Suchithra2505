0d8ce81e1a3942eb2a1352a72848a885f28b6db3
Suchithra2505 <147540982+Suchithra2505@users.noreply.github.com>
2023-12-19 14:22:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+let search= document.getElementById("wrapper");
+let button =document.querySelector("button");
+let input= document.querySelector("input");
+button.addEventListener("click", btnClicked);
+function btnClicked() {
+	search.active;
+	
+	input.focus();
+	
+}
