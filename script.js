f5c4d752b8cc8681757c01416e126f106bcc1c94
Suchithra2505 <147540982+Suchithra2505@users.noreply.github.com>
2023-12-26 14:55:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,8 @@
 //your JS code here. If required.
-document.addEventListener('DOMContentLoaded', function() {
-  // Get elements
-  const searchComponent = document.getElementById('searchComponent');
-  const searchInput = document.getElementById('searchInput');
-  const searchButton = document.getElementById('searchButton');
+function activateSearch() {
+  const searchDiv = document.querySelector('.search');
+  searchDiv.classList.add('active');
 
-  // Function to toggle active state and focus input
-  function activateSearch() {
-    searchComponent.classList.add('active');
-    searchInput.focus();
-  }
-
-  // Event listener for the button click
-  searchButton.addEventListener('click', activateSearch);
-});
+  const inputElement = document.querySelector('.input');
+  inputElement.focus();
+}
